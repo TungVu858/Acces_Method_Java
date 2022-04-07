df560c10e9274602d6f61b6b67f20024761da4c8
TungVu858 <tungvt858@gmail.com>
2022-04-07 08:05:49 +0700

    done

---
+++ src/baitap_xaydung_lopchighi/Students.java
@@ -0,0 +1,28 @@
+package baitap_xaydung_lopchighi;
+
+public class Students {
+    private String name = "Tùng";
+    private String classes = "C0222H1";
+
+    Students() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    public String setName() {
+
+        return this.name;
+    }
+
+    public String setClasses() {
+
+        return this.classes;
+    }
+}
---
+++ src/baitap_xaydung_lopchighi/Test.java
@@ -0,0 +1,11 @@
+package baitap_xaydung_lopchighi;
+
+public class Test {
+    public static void main(String[] args) {
+        Students students = new Students();
+        students.setName();
+        students.setClasses();
+        students.setName("t");
+        students.setClasses("bb");
+    }
+}
